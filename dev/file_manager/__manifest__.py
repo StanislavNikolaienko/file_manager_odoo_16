ee792d06bb6643ff71efc58bd74f62311a9a698a
stanislav <lettersforstanislav@gmail.com>
2023-06-22 11:08:03 +0300

    add linter, fix code style

--- dev/file_manager/__manifest__.py
+++ dev/file_manager/__manifest__.py
@@ -1,16 +1,17 @@
 {
-    'name': 'File Manager 99',
-    'author': 'Nikolaienko Stanislav',
-    'website': 'www.ragnarok.group',
-    'summary': 'File Management Application',
-    'description': 'The File Management Application streamlines file uploading, viewing, and deletion within Odoo. '
-                   'Easily upload files, view a sorted list of uploaded files, and delete unnecessary files. '
-                   'Simplify file management and enhance productivity within your Odoo workflows.',
-    'data': ['views/manager.xml',
-             'security/ir.model.access.csv'],
-    'license': 'AGPL-3',
-
-    'installable': True,
-    'auto_install': False,
-    'application': True,
+    "name": "File Manager 99",
+    "author": "Nikolaienko Stanislav",
+    "website": "www.ragnarok.group",
+    "summary": "File Management Application",
+    "description": (
+        "The File Management Application streamlines file uploading, viewing, and"
+        " deletion within Odoo. Easily upload files, view a sorted list of uploaded"
+        " files, and delete unnecessary files. Simplify file management and enhance"
+        " productivity within your Odoo workflows."
+    ),
+    "data": ["views/manager.xml", "security/ir.model.access.csv"],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "application": True,
 }
